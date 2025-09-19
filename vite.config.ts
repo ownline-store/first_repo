d1fe1b66c450d943a6e8c0f6a8f98e0e3417d670
ownline-store <services@ownlinestore.com>
2025-09-20 01:36:36 +0530

    khupach sundar haay

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/insta_ebook/',
+  base: '/first_repo/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
